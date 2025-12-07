40e16f15812a168431c2f4794083b17638e6cf4d
vasicm4 <vasicmaksim4@gmail.com>
2025-12-07 23:54:55 +0100

    #21 update to themes; add button toggle;

--- graph-visualizer-app/src/app/components/main/header/header.ts
+++ graph-visualizer-app/src/app/components/main/header/header.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {shared} from '../../../app.config';
 import {Router} from '@angular/router';
+import {MatSlideToggleChange} from '@angular/material/slide-toggle';
 
 
 @Component({
@@ -14,6 +15,10 @@ import {Router} from '@angular/router';
 export class Header {
 
   constructor(private router: Router) {
+  }
+
+  onToggleChange(event: MatSlideToggleChange) {
+    console.log('Toggled to:', event.checked ? 'BlockView' : 'Simple View');
   }
 
   navigateToHome() {
