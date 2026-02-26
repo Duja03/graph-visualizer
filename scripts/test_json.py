88797b2e6c1a6c2cbb2c4c4963b37b35026ecde2
sara-stojkov <sarastojkov6@gmail.com>
2026-02-26 17:48:09 +0100

    test: make a more comperhensive test script to test json plugin

--- scripts/test_json.py
+++ scripts/test_json.py
@@ -1,16 +1,42 @@
-from plugins.json_datasource import JsonDataSourcePlugin
-
-plugin = JsonDataSourcePlugin()
-
-graph = plugin.load(
-    file_path="plugins/json_datasource/sample.json"
-)
-
-print("Nodes:", graph.node_count())
-print("Edges:", graph.edge_count())
+from plugins.json_datasource.json_datasource_plugin import JsonDataSourcePlugin
 
 
-print("\nNeighbors of emp_1:")
+def main():
 
-for n in graph.neighbors("emp_1"):
-    print(n.id)
+    plugin = JsonDataSourcePlugin()
+
+    graph = plugin.load(
+        file_path="scripts/sample.json"
+    )
+
+    print("====== GRAPH INFO ======")
+    print("Nodes:", graph.node_count())
+    print("Edges:", graph.edge_count())
+    print()
+
+    print("====== NODES ======")
+
+    for node in graph:
+        print(node.id, node.attributes)
+
+    print()
+    print("====== EDGES ======")
+
+    for edge in graph.iter_edges():
+        print(
+            edge.id,
+            edge.source,
+            "->",
+            edge.target,
+            edge.attributes
+        )
+
+    print()
+    print("====== NEIGHBORS emp_1 ======")
+
+    for n in graph.neighbors("emp_1"):
+        print(n.id)
+
+
+if __name__ == "__main__":
+    main()
