894896e69000ecd172554a5d5009e5cdeed41ddc
sara-stojkov <sarastojkov6@gmail.com>
2026-03-04 19:23:13 +0100

    fix: fix the workspace code and remove duplicate definition

--- platform/src/core/workspace.py
+++ platform/src/core/workspace.py
@@ -1,23 +1,14 @@
 import time
+from api.src.api.model.graph import Graph
+from api.src.api.plugins.datasource_plugin import DataSourcePlugin
+from api.src.api.plugins.visualizer_plugin import VisualizerPlugin
 
-from api.model import Graph
-from api.plugins import DataSourcePlugin
-from api.plugins import VisualizerPlugin
-from plugins.csv_datasource_plugin import CsvDataSourcePlugin
-from plugins.json_datasource import JsonDataSourcePlugin
-from plugins.xml_datasource_plugin import XmlDataSourcePlugin
-
-data_sources = {
-    CsvDataSourcePlugin.static_identifier: CsvDataSourcePlugin,
-    JsonDataSourcePlugin.static_identifier: JsonDataSourcePlugin,
-    XmlDataSourcePlugin.static_identifier: XmlDataSourcePlugin,
-}
 
 class Workspace:
-    def __init__(self):
+    def __init__(self, data_source_plugin: DataSourcePlugin = None):
         self.__id: int = int(time.time())
-        self.__filepath: str | None = ''
-        self.__data_source_plugin: DataSourcePlugin | None = data_sources[JsonDataSourcePlugin.static_identifier]
+        self.__filepath: str = ''
+        self.__data_source_plugin: DataSourcePlugin | None = data_source_plugin
         self.__visualizer_plugin: VisualizerPlugin | None = None
         self.__graph: Graph | None = None
         self.__initial_graph: Graph | None = None
@@ -36,9 +27,24 @@ class Workspace:
 
     @property
     def source_plugin(self) -> DataSourcePlugin:
-        return self.__source_plugin
+        return self.__data_source_plugin
 
     @property
     def visualizer_plugin(self) -> VisualizerPlugin:
         return self.__visualizer_plugin
 
+    @property
+    def graph(self) -> Graph:
+        return self.__graph
+
+    @graph.setter
+    def graph(self, graph: Graph) -> None:
+        self.__graph = graph
+
+    @property
+    def initial_graph(self) -> Graph:
+        return self.__initial_graph
+
+    @initial_graph.setter
+    def initial_graph(self, graph: Graph) -> None:
+        self.__initial_graph = graph
